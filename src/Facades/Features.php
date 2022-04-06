d54edccff6c5715083e73699c5a1048d0c39b267
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-06 19:57:31 +0100

    chore: docblocks

--- src/Facades/Features.php
+++ src/Facades/Features.php
@@ -4,8 +4,15 @@ namespace RyanChandler\LaravelFeatureFlags\Facades;
 
 use Illuminate\Support\Facades\Facade;
 use RyanChandler\LaravelFeatureFlags\FeaturesManager;
+use RyanChandler\LaravelFeatureFlags\Models\Contracts\HasFeatures;
 
 /**
+ * @method static bool enabled(string $name, HasFeatures $for = null)
+ * @method static bool disabled(string $name, HasFeatures $for = null)
+ * @method static void enable(string $name, HasFeatures $for = null)
+ * @method static void disable(string $name, HasFeatures $for = null)
+ * @method static void toggle(string $name, HasFeatures $for = null)
+ *
  * @see \RyanChandler\LaravelFeatureFlags\FeaturesManager
  */
 class Features extends Facade
